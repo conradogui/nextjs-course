3d05221db06f2460e4de21b53bcc6d331ccf7862
conradogui <gui14fbce@gmail.com>
2024-02-22 16:48:40 -0300

    keep studing react

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,17 +1,35 @@
 import Post from "./Post.jsx";
-import NewPost from './NewPost'
-import classes from './PostsList.module.css'
+import NewPost from "./NewPost";
+import classes from "./PostsList.module.css";
+import { useState } from "react";
+import Modal from "./Modal.jsx";
 
 function PostsList() {
-    return (
-        <>
-        <NewPost/>
-            <ul className={classes.posts}>
-                <li><Post author="Guilherme" body='learning react'/></li>
-                <li><Post author="Mariana" body='learning vue'/></li>
-            </ul>
-        </>
-    )
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <Modal>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+      </Modal>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Mariana" body="learning vue" />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
